8e04d6ea41cd182992af2bf55ff15fbe564fd707
Jacob Lower <jlower1@gmail.com>
2017-07-25 23:46:24 -0700

    color setup

---
+++ Actual/color_setup/CMakeFiles/color_setup.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/color_setup.dir/color_setup.cpp.o"
+  "color_setup.pdb"
+  "color_setup"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/color_setup.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
